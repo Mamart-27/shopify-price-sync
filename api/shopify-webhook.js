14c1dc5474b5d796b34d4b97bdd7c54711f87fbb
Jomart <jaguiman27@gmail.com>
2025-06-05 16:30:11 +0800

    Update shopify-webhook.js

--- api/shopify-webhook.js
+++ api/shopify-webhook.js
@@ -1,3 +1,12 @@
+module.exports = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).send('Method Not Allowed');
+  }
+
+  console.log("🔥 Incoming Shopify webhook:", JSON.stringify(req.body, null, 2));
+
+  // ... rest of your code
+
 const axios = require('axios');
 const getRawBody = require('raw-body');
 
